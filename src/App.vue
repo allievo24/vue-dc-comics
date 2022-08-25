5a3c524d9f6e0bb9ba00f6effffe981edeb7796d
saidmoussadek <saiddus24@gmail.com>
2022-08-25 02:32:19 +0200

    Inserimento Contenuti Header

--- src/App.vue
+++ src/App.vue
@@ -33,6 +33,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;*/
-  background: blueviolet;
+  
 }
+  @import "./styles/general.scss"
 </style>
--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,27 +1,79 @@
 <!--creo il componente Header-->
 <template>
-    <header>
-        <img src="../assets/img/dc-logo.png" alt="logo"/>
-        <nav>
-            <ul>
-                <li>
-                    <a href="#"></a>
-                </li>
-            </ul>
-        </nav>
-        <div class="Bottom">
-            -->Content goes here --
-        </div>
-    </header>
-  
+  <header>
+    <img src="../assets/img/dc-logo.png" alt="logo" />
+    <nav>
+      <ul>
+        <li v-for="(link,index) in links" :key="index">
+          <a :href="link.url">{{link.text}}</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="Bottom">-->Content goes here --</div>
+  </header>
 </template>
 
 <script>
 export default {
-        name: 'MyHeader'
-}
+  name: "MyHeader",
+  data() {
+    return {
+      links: [
+        {
+          text: "CHARACTERS",
+          url: "#",
+          active: false,
+        },
+        {
+          text: "COMICS",
+          url: "#",
+          active: true,
+        },
+        {
+          text: "MOVIES",
+          url: "#",
+          active: false,
+        },
+        {
+          text: "TV",
+          url: "#",
+          actve: false,
+        },
+        {
+          text: "GAMES",
+          url: "#",
+          actve: false,
+        },
+        {
+          text: "COLLECTIBLES",
+          url: "#",
+          actve: false,
+        },
+        {
+          text: "VIDEOS",
+          url: "#",
+          actve: false,
+        },
+        {
+          text: "FANS",
+          url: "#",
+          actve: false,
+        },
+        {
+          text: "NEWS",
+          url: "#",
+          actve: false,
+        },
+        {
+          text: "SHOP",
+          url: "#",
+          actve: false,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
